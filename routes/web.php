fa61ad27fc8df6416dbe4cfcfb194bcae09e5e31
Jonas Gabriel <jgalmeida1993@gmail.com>
2020-02-24 10:31:27 -0300

    :pencil2: Remove dependencia do middleware na rota

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/usuarios', 'UsuarioControlador@index')->
-       middleware('primeiro');
+Route::get('/usuarios', 'UsuarioControlador@index');
